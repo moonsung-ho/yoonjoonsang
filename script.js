9d563723f4878af884934b3a2df033f5ec561b00
moonsung-ho <sungho.moon@aol.com>
2023-12-05 01:45:41 +0900

    Add share button for social media sharing

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ let thanksto = document.querySelector("#thanksto");
 observer.observe(about);
 observer.observe(promises);
 observer.observe(thanksto);
+
+if (typeof navigator.share === "undefined") {
+  // 공유하기 버튼을 지원하지 않는 경우에 대한 폴백 처리
+  document.querySelector("#share").hidden = true;
+} else {
+  document.querySelector("#share").addEventListener("click", () => {
+    navigator.share({
+      title: "전교 회장 후보 윤준상", // 공유될 제목
+      url: "https://joonsang.netlify.app", // 공유될 URL
+    });
+  });
+}
